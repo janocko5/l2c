29369425907a60ebd28433704c3e4afc45f980fb
janocko5 <miroslav.janocko8@gmail.com>
2020-05-01 18:58:48 +0200

    Sprint start

---
+++ src/main/java/com/janocko/demo/Janockol2cApplication.java
@@ -0,0 +1,13 @@
+package com.janocko.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Janockol2cApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Janockol2cApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/janocko/demo/Janockol2cApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janocko.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Janockol2cApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
